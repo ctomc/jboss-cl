0db42d79ab5cc4e2c41d76b7cbc4d30ff41c3a8f
Adrian Brock <warjort@gmail.com>
2009-01-27 10:43:38 +0000

    Fixup this test making bad assumptions
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/jboss-cl/trunk@83458 84be2c1e-ba19-0410-b317-a758671a6fc1

--- classloading-vfs/src/test/java/org/jboss/test/classloading/vfs/metadata/test/GeneratedClassesUnitTestCase.java
+++ classloading-vfs/src/test/java/org/jboss/test/classloading/vfs/metadata/test/GeneratedClassesUnitTestCase.java
@@ -89,11 +89,11 @@ public class GeneratedClassesUnitTestCase extends VFSClassLoadingMicrocontainerT
       ClassLoadingMetaDataFactory factory = ClassLoadingMetaDataFactory.getInstance();
       String dynamicClassRoot = getDynamicClassRoot();
       VFSClassLoaderFactory a = new VFSClassLoaderFactory("a");
-      a.setExportAll(ExportAll.NON_EMPTY);
       a.setImportAll(true);
       a.getRoots().add(getRoot(A.class));
       a.getRoots().add(dynamicClassRoot);
       a.getCapabilities().addCapability(factory.createPackage(A.class.getPackage().getName()));
+      a.getCapabilities().addCapability(factory.createPackage(info.getPackageName()));
       KernelDeployment depA = install(a);
 
       VFSClassLoaderFactory b = new VFSClassLoaderFactory("b");
@@ -160,6 +160,11 @@ public class GeneratedClassesUnitTestCase extends VFSClassLoadingMicrocontainerT
       public String getClassname()
       {
          return classname;
+      }
+      
+      public String getPackageName()
+      {
+         return ClassLoaderUtils.getClassPackageName(classname);
       }
 
       public String getResourceName()
